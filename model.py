8448552d93ae904297c730e8c0a7db560cc77588
Kaan Yilmaz <kaanyilmaz@ymail.com>
2020-04-08 20:47:41 -0400

    building predict

--- model.py
+++ model.py
@@ -34,7 +34,7 @@ class ANN():
 
         #np.ones((len(X),1))
         #self.bias_term = [1]*(self.L-1)
-
+        self.deriv = lambda a: a * (1.0 - a)
         #Note: columns (feautures) represent neurons
         self.w = [np.random.rand(len(X[0]),units_count_hidden_layer[0]+1)] # old + 1 * new (the +1 is for the bias term)
         for i in range(len(units_count_hidden_layer)-1):
@@ -42,48 +42,38 @@ class ANN():
         self.w.append(np.random.rand(units_count_hidden_layer[-1]+1,len(Y[0])))
         # size of w is L -1
         #len(numpy) gives number of rows
-        self.act_units = [] # will be the same size as w eventually (the input units "have already been activated")
 
     def fit(self, alpha, epochs, activation = "sigmoid"):
         for i in range(epochs):
             print("Starting Epoch #"+str(i+1))
             print()
-            self.mini_batch(alpha, activation)
+            self.mini_batch(alpha,activation)
 
     def mini_batch(self,alpha,activation):
         for i in range(self.batch):
-            self.fwd_prop(i, activation)
-            self.back_prop(i, alpha)
+            act_units = [self.X[i]] # will be the same size as w eventually (the input units "have already been activated")
+            self.fwd_prop(self.X[i], act_units , activation)
+            self.back_prop(i,act_units,alpha)
 
-    def fwd_prop(self,batch,activation):
+    def fwd_prop(self,batch,act_units, activation):
         print("----Feed-forward starting")
         for l in range(1,self.L):
             print("Activating Layer #"+str(l))
-            new_layer = self.activate_layer(l,batch, activation)
-            self.act_units.append(new_layer)
+            new_layer = self.activate_layer(l,batch,act_units,activation)
+            act_units.append(new_layer)
         print()
 
-    def back_prop(self,batch,alpha):
-        print("----Back-propagation starting")
-        # total of L-1 updates to gradient
-        dz = self.act_units[-1]-self.Y[batch] #self.act_units[-1] is our "y_hat"
-        dw = np.dot( self.act_units[-2].T, dz )/len(self.X)
-        self.w[-1] -= alpha * dw # + reg
-        for l in range(2,self.L):
-            print("Updating Layer #"+str(-l))
-            dz = np.dot( dz , self.w[-l+1].T) * self.act_deriv(self.act_units[-l])
-            dw = np.dot( self.act_units[-(l+1)].T, dz )/len(self.X)
-            self.w[-l] -= alpha * dw # + reg
-    def get_z(self,layer,batch): # layer is the layer we are creating 1<layer<L (not zero)
-        if layer == 1:
-            self.act_units = [self.X[batch]]
-            return np.dot(self.X[batch],self.w[layer-1])
-        else:
-            return np.dot(self.act_units[-1],self.w[layer-1])
-
-    def activate_layer(self,layer,batch, activation):
-        if activation == "sigmoid":
-            act = 1/(1+np.exp(-self.get_z(layer,batch)) )
+    def activate_layer(self,layer,batch,act_units, activation = "sigmoid"):
+        if activation =="ReLU":
+            print("do this")
+        elif activation =="softmax": #fix this
+            t = np.exp(self.get_z(layer,batch,act_units))
+            denom = np.sum(t,axis=1,keepdims=True)
+            vfunc = np.vectorize(lambda x : x/denom)
+            act = vfunc()
+        else: #Sigmoid
+            act = 1/(1+np.exp(-self.get_z(layer,batch,act_units) ))
+        print()
         #if layer<self.L-1: # incorporate the bias term now for the next layer (adding a bias col/cell to every row) adding a bias neuron
             #bias = np.full( (len(act),1),self.bias_term[layer]) # this value is edited in backprop so must
             #bias = np.ones((len(act),1))
@@ -91,12 +81,49 @@ class ANN():
         #else:
         return act
 
-    def act_deriv(self,func):
-        return func * (1.0 - func)
+    def get_z(self,layer,batch,act_units): # layer is the layer we are creating 1<layer<L (not zero)
+        if layer == 1:
+            return np.dot(batch,self.w[layer-1])
+        else:
+            return np.dot(act_units[layer-1],self.w[layer-1])
+
+
+    def back_prop(self,batch,act_units,alpha):
+        print("----Back-propagation starting")
+        # total of L-1 updates to gradient
+        dz = act_units[-1]-self.Y[batch] #self.act_units[-1] is our "y_hat"
+
+        dw = np.dot( act_units[-2].T, dz )/len(self.X[batch])
+        self.w[-1] -= alpha * dw # + reg
+        for l in range(2,self.L):
+            print("Updating Layer #"+str(-l))
+            dz = np.dot( dz , self.w[-l+1].T) * self.deriv(act_units[-l])
+            dw = np.dot( act_units[-(l+1)].T, dz )/len(self.X[batch])
+            self.w[-l] -= alpha * dw # + reg
+
+    def predict(self,x_test,y_test,activation="sigmoid"):
+        prediction = [x_test]
+        for l in range(1,self.L):
+            new_layer = self.activate_layer(l,x_test,prediction)
+            prediction.append(new_layer)
+        pred = prediction[-1]
+        if activation =="ReLU":
+            print("do this")
+        elif activation =="softmax": #fix this
+            return np.max(pred,axis=1)
+        else: #Sigmoid
+            best = np.max(pred,axis=1)
+            vfunc = np.vectorize(lambda x : 1 if (x == best) else 0)
+            return vfunc(pred)
+
+    def eval_acc(self,pred,y):
+        return (pred == y)
+
 
 if __name__ == "__main__":
     (x_train, y_train), (x_test, y_test) = get_data()
-    NN = ANN(x_train,y_train,[50,10],5)
+    NN = ANN(x_train,y_train,[3],2)
     #NN2 = ANN(x_train,y_train,[50,40],5)
 
     NN.fit(0.1,2)
+    print(NN.predict(x_test, y_test))
